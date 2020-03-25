7d103c301212ff2fdd6909dd7ea35278fee70d0d
edmundo papi <edmundopierre@yahoo.com>
2020-03-25 01:13:19 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,14 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+
+  while number >= 1
+    sleep 2
+    puts "#{number} SECOND(S)!"
+    number -=1
+    #binding.pry
+  end
+
+end
