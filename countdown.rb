acc7a2546b4d5e8ad8446216565b24e4e368ae42
edmundo papi <edmundopierre@yahoo.com>
2020-03-25 01:07:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@
 require 'pry'
 def countdown(number)
 
-  while number <= 10
+  while number >= 1
     puts "#{number} SECOND(S)!"
-    number +=1
+    number -=1
     #binding.pry
   end
   return "HAPPY NEW YEAR!"
